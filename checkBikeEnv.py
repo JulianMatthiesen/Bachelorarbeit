5ae0402644e1f80f0b2ea1e3c7913404b796c51e
JulianMatthiesen <118197648+JulianMatthiesen@users.noreply.github.com>
2023-10-13 11:09:39 +0200

    Delete checkBikeEnv.py

--- checkBikeEnv.py
+++
@@ -1,8 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from GoToGoalBikeEnv import BikeEnv
-from gym.wrappers import FlattenObservation
-
-
-env = BikeEnv()
-env = FlattenObservation(env)
-check_env(env)
